61aee3d205931963f1248b81a6f101bd8c1f717d
hadicoffee <hadikusumaandroid@gmail.com>
2013-03-09 07:12:16 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,7 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := vendor/allwinner/a10/recovery_keys.c
 # TARGET_RECOVERY_INITRC := recovery_init.rc
 
 TARGET_PREBUILT_KERNEL := device/allwinner/a10/kernel
