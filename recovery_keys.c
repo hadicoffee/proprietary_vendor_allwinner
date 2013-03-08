c77372f9b33b4e218118e81fe69a49a54aede073
hadicoffee <hadikusumaandroid@gmail.com>
2013-03-09 07:59:52 +0800

    Update recovery_keys.c

--- recovery_keys.c
+++ recovery_keys.c
@@ -11,26 +11,26 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
 
 int device_handle_key(int key_code, int visible) {
     if (visible) {
-        switch (key_code) {
-            case 102:
+                switch (key_code) {
+case 102:
 case 217:
 case 114:
                 return HIGHLIGHT_DOWN;
 
-            case 115:
+case 115:
 case 139:
                 return HIGHLIGHT_UP;
 
-            case KEY_2:
-            case KEY_POWER:
+case KEY_2:
+case KEY_POWER:
                 return SELECT_ITEM;
             
 case 158:
 case KEY_1:
-            case KEY_ESC:
-                    return GO_BACK;
+case KEY_ESC:
+                return GO_BACK;
         }
     }
 
-    return NO_ACTION;
+                return NO_ACTION;
 }
