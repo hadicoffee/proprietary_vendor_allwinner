ef638e8b1bddccc1b95a78b3ce2a8f2bdf197e90
hadicoffee <hadikusumaandroid@gmail.com>
2013-03-09 21:28:57 +0800

    Update BoardConfig.mk

--- a13/BoardConfig.mk
+++ a13/BoardConfig.mk
@@ -26,18 +26,6 @@ BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/fi
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := vendor/allwinner/a10/recovery_keys.c
 # TARGET_RECOVERY_INITRC := vendor/allwinner/a10/recovery_init.rc
 
-#twrp recovery
-#BOARD_HAS_SDCARD_INTERNAL := true
-#TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-#DEVICE_RESOLUTION := 1024x768
-#TW_FLASH_FROM_STORAGE := true
-#TW_NO_REBOOT_BOOTLOADER := true
-#TWRP_EVENT_LOGGING := true
-#TW_INTERNAL_STORAGE_PATH := "/emmc"
-#TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
-#TW_EXTERNAL_STORAGE_PATH := "/sdcard"
-#TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
-#BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file
 
 TARGET_PREBUILT_KERNEL := device/allwinner/a10/kernel
 
